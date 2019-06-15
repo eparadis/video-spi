4ffe324de90bfa895fe5ee6611bf1806c7ae2a6c
Ed Paradis <eparadis@users.noreply.github.com>
2019-06-15 05:16:06 -0700

    move interrupt-related stuff to its own file

--- VideoSPI.ino
+++ VideoSPI.ino
@@ -15,6 +15,7 @@
 #include "params.h"
 #include "text.h"
 #include "scanlines.h"
+#include "interrupts.h"
 
 char videomem[VID_WIDTH*VID_HEIGHT];
 //
@@ -31,24 +32,9 @@ void setup(void)
 {
   // sleep for a bit to increase the chances of the bootloader working for reprogramming
   delay(2000);
-  
-  cli();
-  UBRR0 = 0; // must be zero before enabling the transmitter
-  XCK0_DDR  |= _BV(XCK0); // set XCK pin as output to enable master mode
-  UCSR0C     = _BV (UMSEL00) | _BV (UMSEL01);  // SPI master mode
-  DDR_VID   |= _BV(VID_PIN);
-  DDR_SYNC  |= _BV(SYNC_PIN);
-  PORT_VID  &= ~_BV(VID_PIN);
-  PORT_SYNC |= _BV(SYNC_PIN);
-  TCCR1A     = _BV(COM1A1) | _BV(COM1A0) | _BV(WGM11); // inverted fast pwm mode on timer 2
-  TCCR1B     = _BV(WGM13) | _BV(WGM12) | _BV(CS10);
-  ICR1       = _NTSC_CYCLES_SCANLINE;
-  OCR1A      = _CYCLES_HSYNC;
-  OCR1B      = _NTSC_CYCLES_OUTPUT_START - 79;
-  TIMSK1     = _BV(OCIE1B);
-  TIMSK0     = 0; // turn timer0 off!
-  SMCR       = _BV(SE); // allow IDLE sleep mode
-  sei();
+
+  setupVideoInterrupts();
+
   //
   // Clear the video buffer and print out sample text
   //
@@ -60,19 +46,6 @@ void setup(void)
   box(0, 9, 19, 15);
   clrrect(20,9, 19, 15, 0);
   box(20,9,19,15);
-}
-//
-// Handle scanline timing
-//
-ISR(TIMER1_COMPA_vect)
-{
-  sei();
-  asm("sleep\n"); // for clock synchronization during active lines
-}
-ISR(TIMER1_COMPB_vect)
-{
-  line_handler();
-  scanline++;
 }
 
 /*
---
+++ interrupts.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include "interrupts.h"
+#include "scanlines.h"
+
+extern void (*line_handler)(void);
+extern int scanline;
+
+//
+// Handle scanline timing
+//
+ISR(TIMER1_COMPA_vect)
+{
+  sei();
+  asm("sleep\n"); // for clock synchronization during active lines
+}
+ISR(TIMER1_COMPB_vect)
+{
+  line_handler();
+  scanline++;
+}
+
+void setupVideoInterrupts() {
+  cli();
+  UBRR0 = 0; // must be zero before enabling the transmitter
+  XCK0_DDR  |= _BV(XCK0); // set XCK pin as output to enable master mode
+  UCSR0C     = _BV (UMSEL00) | _BV (UMSEL01);  // SPI master mode
+  DDR_VID   |= _BV(VID_PIN);
+  DDR_SYNC  |= _BV(SYNC_PIN);
+  PORT_VID  &= ~_BV(VID_PIN);
+  PORT_SYNC |= _BV(SYNC_PIN);
+  TCCR1A     = _BV(COM1A1) | _BV(COM1A0) | _BV(WGM11); // inverted fast pwm mode on timer 2
+  TCCR1B     = _BV(WGM13) | _BV(WGM12) | _BV(CS10);
+  ICR1       = _NTSC_CYCLES_SCANLINE;
+  OCR1A      = _CYCLES_HSYNC;
+  OCR1B      = _NTSC_CYCLES_OUTPUT_START - 79;
+  TIMSK1     = _BV(OCIE1B);
+  TIMSK0     = 0; // turn timer0 off!
+  SMCR       = _BV(SE); // allow IDLE sleep mode
+  sei();
+}
